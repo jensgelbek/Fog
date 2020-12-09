4fab3c246fb54998b3f84eb56b132eddbf189904
jensgelbek <jensgelbek@gmail.com>
2020-12-09 14:59:46 +0100

    nu kan seller oprettes (opretSeller) og logge ind(i dropdown) skal lige lave det så password "1234" skal ændres.

---
+++ src/main/resources/migrate/5.sql
@@ -0,0 +1,232 @@
+
+-- stolpe
+INSERT INTO materialer (id,name,details,pris) VALUES (1,'stolpe','97x97 mm. trykimp. Stolpe',40);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (1,3000);
+
+-- spær/rem
+INSERT INTO materialer (id,name,details,pris) VALUES (21,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (21,2400);
+INSERT INTO materialer (id,name,details,pris) VALUES (22,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (22,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (23,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (23,3000);
+INSERT INTO materialer (id,name,details,pris) VALUES (24,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (24,3300);
+INSERT INTO materialer (id,name,details,pris) VALUES (25,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (25,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (26,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (26,3900);
+INSERT INTO materialer (id,name,details,pris) VALUES (27,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (27,4200);
+INSERT INTO materialer (id,name,details,pris) VALUES (28,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (28,4500);
+INSERT INTO materialer (id,name,details,pris) VALUES (29,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (29,4800);
+INSERT INTO materialer (id,name,details,pris) VALUES (30,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (30,5100);
+INSERT INTO materialer (id,name,details,pris) VALUES (31,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (31,5400);
+INSERT INTO materialer (id,name,details,pris) VALUES (32,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (32,5700);
+INSERT INTO materialer (id,name,details,pris) VALUES (33,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (33,6000);
+INSERT INTO materialer (id,name,details,pris) VALUES (34,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (34,6300);
+INSERT INTO materialer (id,name,details,pris) VALUES (35,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (35,6600);
+INSERT INTO materialer (id,name,details,pris) VALUES (36,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (36,6900);
+INSERT INTO materialer (id,name,details,pris) VALUES (37,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (37,7200);
+INSERT INTO materialer (id,name,details,pris) VALUES (38,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (38,7500);
+INSERT INTO materialer (id,name,details,pris) VALUES (39,'spær/rem','45x195 mm. spærtræ ubh.',70);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (39,7800);
+
+-- understern
+INSERT INTO materialer (id,name,details,pris) VALUES (51,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (51,2400);
+INSERT INTO materialer (id,name,details,pris) VALUES (52,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (52,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (53,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (53,3000);
+INSERT INTO materialer (id,name,details,pris) VALUES (54,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (54,3300);
+INSERT INTO materialer (id,name,details,pris) VALUES (55,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (55,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (56,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (56,3900);
+INSERT INTO materialer (id,name,details,pris) VALUES (57,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (57,4200);
+INSERT INTO materialer (id,name,details,pris) VALUES (58,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (58,4500);
+INSERT INTO materialer (id,name,details,pris) VALUES (59,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (59,4800);
+INSERT INTO materialer (id,name,details,pris) VALUES (60,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (60,5100);
+INSERT INTO materialer (id,name,details,pris) VALUES (61,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (61,5400);
+INSERT INTO materialer (id,name,details,pris) VALUES (62,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (62,5700);
+INSERT INTO materialer (id,name,details,pris) VALUES (63,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (63,6000);
+INSERT INTO materialer (id,name,details,pris) VALUES (64,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (64,6300);
+INSERT INTO materialer (id,name,details,pris) VALUES (65,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (65,6600);
+INSERT INTO materialer (id,name,details,pris) VALUES (66,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (66,6900);
+INSERT INTO materialer (id,name,details,pris) VALUES (67,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (67,7200);
+INSERT INTO materialer (id,name,details,pris) VALUES (68,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (68,7500);
+INSERT INTO materialer (id,name,details,pris) VALUES (69,'understern','25x200 mm. trykimp. Brædt',30);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (69,7800);
+
+-- overstern
+INSERT INTO materialer (id,name,details,pris) VALUES (71,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (71,2400);
+INSERT INTO materialer (id,name,details,pris) VALUES (72,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (72,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (73,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (73,2500);
+INSERT INTO materialer (id,name,details,pris) VALUES (74,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (74,3300);
+INSERT INTO materialer (id,name,details,pris) VALUES (75,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (75,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (76,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (76,3900);
+INSERT INTO materialer (id,name,details,pris) VALUES (77,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (77,4200);
+INSERT INTO materialer (id,name,details,pris) VALUES (78,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (78,4500);
+INSERT INTO materialer (id,name,details,pris) VALUES (79,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (79,4800);
+INSERT INTO materialer (id,name,details,pris) VALUES (80,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (80,5100);
+INSERT INTO materialer (id,name,details,pris) VALUES (81,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (81,5400);
+INSERT INTO materialer (id,name,details,pris) VALUES (82,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (82,5700);
+INSERT INTO materialer (id,name,details,pris) VALUES (83,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (83,6000);
+INSERT INTO materialer (id,name,details,pris) VALUES (84,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (84,6300);
+INSERT INTO materialer (id,name,details,pris) VALUES (85,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (85,6600);
+INSERT INTO materialer (id,name,details,pris) VALUES (86,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (86,6900);
+INSERT INTO materialer (id,name,details,pris) VALUES (87,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (87,7200);
+INSERT INTO materialer (id,name,details,pris) VALUES (88,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (88,7500);
+INSERT INTO materialer (id,name,details,pris) VALUES (89,'overstern','25x125mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (89,7800);
+
+-- brædt
+INSERT INTO materialer (id,name,details,pris) VALUES (100,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (100,1800);
+INSERT INTO materialer (id,name,details,pris) VALUES (101,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (101,2100);
+INSERT INTO materialer (id,name,details,pris) VALUES (102,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (102,2400);
+INSERT INTO materialer (id,name,details,pris) VALUES (103,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (103,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (104,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (104,2500);
+INSERT INTO materialer (id,name,details,pris) VALUES (105,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (105,3300);
+INSERT INTO materialer (id,name,details,pris) VALUES (106,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (106,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (107,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (107,3900);
+INSERT INTO materialer (id,name,details,pris) VALUES (108,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (108,4200);
+INSERT INTO materialer (id,name,details,pris) VALUES (109,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (109,4500);
+INSERT INTO materialer (id,name,details,pris) VALUES (110,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (110,4800);
+INSERT INTO materialer (id,name,details,pris) VALUES (111,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (111,5100);
+INSERT INTO materialer (id,name,details,pris) VALUES (112,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (112,5400);
+INSERT INTO materialer (id,name,details,pris) VALUES (113,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (113,5700);
+INSERT INTO materialer (id,name,details,pris) VALUES (114,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (114,6000);
+INSERT INTO materialer (id,name,details,pris) VALUES (115,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (115,6300);
+INSERT INTO materialer (id,name,details,pris) VALUES (116,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (116,6600);
+INSERT INTO materialer (id,name,details,pris) VALUES (117,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (117,6900);
+INSERT INTO materialer (id,name,details,pris) VALUES (118,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (118,7200);
+INSERT INTO materialer (id,name,details,pris) VALUES (119,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (119,7500);
+INSERT INTO materialer (id,name,details,pris) VALUES (120,'brædt','19x100 mm. trykimp. Brædt',25);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (120,7800);
+
+-- reglar
+INSERT INTO materialer (id,name,details,pris) VALUES (100,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (100,1800);
+INSERT INTO materialer (id,name,details,pris) VALUES (101,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (101,2100);
+INSERT INTO materialer (id,name,details,pris) VALUES (102,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (102,2400);
+INSERT INTO materialer (id,name,details,pris) VALUES (103,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (103,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (104,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (104,2700);
+INSERT INTO materialer (id,name,details,pris) VALUES (105,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (105,3300);
+INSERT INTO materialer (id,name,details,pris) VALUES (106,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (106,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (107,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (107,3900);
+INSERT INTO materialer (id,name,details,pris) VALUES (108,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (108,4200);
+INSERT INTO materialer (id,name,details,pris) VALUES (109,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (109,4500);
+INSERT INTO materialer (id,name,details,pris) VALUES (110,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (110,4800);
+INSERT INTO materialer (id,name,details,pris) VALUES (111,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (111,5100);
+INSERT INTO materialer (id,name,details,pris) VALUES (112,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (112,5400);
+INSERT INTO materialer (id,name,details,pris) VALUES (113,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (113,5700);
+INSERT INTO materialer (id,name,details,pris) VALUES (114,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (114,6000);
+INSERT INTO materialer (id,name,details,pris) VALUES (115,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (115,6300);
+INSERT INTO materialer (id,name,details,pris) VALUES (116,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (116,6600);
+INSERT INTO materialer (id,name,details,pris) VALUES (117,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (117,6900);
+INSERT INTO materialer (id,name,details,pris) VALUES (118,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (118,7200);
+INSERT INTO materialer (id,name,details,pris) VALUES (119,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (119,7500);
+INSERT INTO materialer (id,name,details,pris) VALUES (120,'reglar','45x95 mm. Reglar ub.',27);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (120,7800);
+
+-- trapez plader
+INSERT INTO materialer (id,name,details,pris) VALUES (130,'trapez','Plastmo Ecolite blåtonet',40);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (130,3600);
+INSERT INTO materialer (id,name,details,pris) VALUES (131,'trapez','Plastmo Ecolite blåtonet',40);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (131,6000);
+
+-- lægte
+INSERT INTO materialer (id,name,details,pris) VALUES (132,'lægte','38x73 mm. Lægte ubh.',42);
+INSERT INTO volumematerialer (volumeMaterialeId,length) VALUES (132,3600);
+
+-- ----------------------------
+-- Records of properties
+-- ----------------------------
+BEGIN;
+INSERT INTO `properties` VALUES ('version', '5');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS=1;
+
