6fee5c746aa92329f3e19180c74d25a85e4f0397
Peter-Rambeck <peter.rambeck@me.com>
2020-12-10 16:29:51 +0100

    Nu

---
+++ src/main/java/domain/materials/Stykliste.java
@@ -0,0 +1,29 @@
+package domain.materials;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stykliste {
+
+
+    private List<StykListeLinje> stykListeLinjer = new ArrayList<>();
+    // private List<unitMaterial> unitMaterials = new ArrayList<>();
+
+
+/*
+    public static StykListeLinje spaerCalc(int width, int length) {
+        StykListeLinje spaer;
+        String name = "Spær: Monteres på rem";
+        Double spaerLength = Double.valueOf(length);
+        int unit = (int) (length / 60.0);
+        int price = 50;
+        int sum = (int) ((spaerLength/100) * price * unit);
+        spaer = new StykListeLinje();
+        System.out.println(spaer);
+        return spaer;
+    }
+
+ */
+
+
+}
