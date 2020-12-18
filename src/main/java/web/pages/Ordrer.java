632f67f5fe52fce66383c2d32dce868aeb0c6f82
jensgelbek <jensgelbek@gmail.com>
2020-12-18 09:25:12 +0100

    nu kan antal ændres i stykliste...

--- src/main/java/web/pages/Ordrer.java
+++ src/main/java/web/pages/Ordrer.java
@@ -76,7 +76,7 @@ public class Ordrer extends BaseServlet {
             int orderToShow = Integer.parseInt(req.getParameter("vis"));
 
             // System.out.println(orderToShow);
-            nextPage="/minOrdre?ordre=" + orderToShow;
+            nextPage="/ordre?ordre=" + orderToShow;
 
         }
         if (req.getParameter("afslut") != null) {
